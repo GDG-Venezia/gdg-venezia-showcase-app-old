df2f9faa8324d65f7bd80bb5b068a6399584da58
Marco Gomiero <marco.gomiero.93@gmail.com>
2019-08-15 23:13:21 +0200

    Set social view

--- iosApp/iosApp/Source/ContactsViewController.swift
+++ iosApp/iosApp/Source/ContactsViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ContactsViewController: UIViewController {
 
     @IBOutlet weak var labelToolbar: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // TODO: change
+    private var socialList = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,17 +22,48 @@ class ContactsViewController: UIViewController {
         
         self.labelToolbar.text = "GDG Venezia"
         self.labelToolbar.font = Fonts.get(.regular, size: Fonts.Sizes.xLarge)
+        
+        let eventCell = UINib(nibName: "IconImageCell", bundle: nil)
+        self.tableView.register(eventCell, forCellReuseIdentifier: "SocialCell")
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.backgroundColor = Colors.backgroundColor
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.separatorStyle = .none
+    }
+}
+
+extension ContactsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SocialCell", for: indexPath) as! IconImageCell
+        cell.backgroundColor = UIColor.clear
+        cell.selectionStyle = .none
+        
+        // TODO: add missing data
+        cell.setupSocialCell()
+        
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // Cell shadow stuff
+        cell.contentView.layer.masksToBounds = true
+        let radius = cell.contentView.layer.cornerRadius
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: radius).cgPath
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.socialList.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("item selected")
+    }
 }
