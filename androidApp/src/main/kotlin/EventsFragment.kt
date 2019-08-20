4f71acb6fd3125a3d2b3befbb7906469d7520095
Marco Gomiero <marco.gomiero.93@gmail.com>
2019-08-20 23:44:55 +0200

    Implement Events UI

--- androidApp/src/main/kotlin/EventsFragment.kt
+++ androidApp/src/main/kotlin/EventsFragment.kt
@@ -4,14 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.github.gdgvenezia.ServiceLocator
 import com.github.gdgvenezia.domain.entities.EventListModel
 import com.github.gdgvenezia.presentation.events.EventListView
+import com.google.android.material.snackbar.Snackbar
 
-class EventsFragment: Fragment(), EventListView {
+
+class EventsFragment : Fragment(), EventListView {
+
+    private lateinit var progress: ProgressBar
+    private lateinit var pastRecyclerView: RecyclerView
+    private lateinit var futureRecyclerView: RecyclerView
+    private lateinit var listGroup: Group
+    private lateinit var mainView: ConstraintLayout
+    private lateinit var futureTitle: TextView
+
+    private val pastEventsAdapter = EventsAdapter(mutableListOf())
+    private val futureEventsAdapter = EventsAdapter(mutableListOf())
 
     private val presenter by lazy { ServiceLocator.eventListPresenter }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -20,18 +38,45 @@ class EventsFragment: Fragment(), EventListView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        progress = view.findViewById(R.id.EVENTS_progress)
+        pastRecyclerView = view.findViewById(R.id.EVENTS_past_list)
+        futureRecyclerView = view.findViewById(R.id.EVENTS_future_list)
+        listGroup = view.findViewById(R.id.EVENTS_list_group)
+        mainView = view.findViewById(R.id.EVENTS_constraint_layout)
+        futureTitle = view.findViewById(R.id.EVENTS_future_title)
+
+        futureRecyclerView.isNestedScrollingEnabled = false
+        pastRecyclerView.isNestedScrollingEnabled = false
     }
 
     override fun renderEventList(eventList: EventListModel) {
-//        text.text = eventList.futureEvents.plus(eventList.pastEvents).format()
+        if (eventList.futureEvents.isNullOrEmpty()) {
+            futureTitle.visibility = View.GONE
+            futureRecyclerView.visibility = View.GONE
+        } else {
+            futureEventsAdapter.items = eventList.futureEvents
+            futureRecyclerView.adapter = futureEventsAdapter
+        }
+
+        pastEventsAdapter.items = eventList.pastEvents
+        pastRecyclerView.adapter = pastEventsAdapter
+
+        pastEventsAdapter.notifyDataSetChanged()
+        futureEventsAdapter.notifyDataSetChanged()
     }
 
     override fun renderError(errorMessage: String) {
-//        text.text = errorMessage
+       Snackbar.make(mainView, errorMessage, Snackbar.LENGTH_LONG).show()
     }
 
     override fun renderLoading(visible: Boolean) {
-//        progress.visibility = if (visible) View.VISIBLE else View.GONE
+        if (visible) {
+            listGroup.visibility = View.GONE
+            progress.visibility = View.VISIBLE
+        } else {
+            listGroup.visibility = View.VISIBLE
+            progress.visibility = View.GONE
+        }
     }
 
     override fun onStart() {
